3b98b7c91005b69609f98c40810f14c9305297e4
HacheDev <hache.dev16@gmail.com>
2023-02-07 19:09:10 +0100

    Adding ts interfaces for chat component

--- components/Chat.vue
+++ components/Chat.vue
@@ -1,3 +1,16 @@
 <template>
-    
+    <div class="chat-header">Sorting Hat</div>
+    <div class="chat-container">
+        <div class="chat-message-list" v-for="question in questions" :key="question.title">
+
+        </div>
+    </div>
 </template>
+
+<script lang="ts" setup>
+import * as questions from "~/assets/sorting_hat.json"
+let currentMessageIndex = useState<number>("currentMessageIndex", () => 0)
+
+
+
+</script>
